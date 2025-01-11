4ca8c0a06e0f9d42a4804a13d096f388db9d9f2e
peter-savron <peter.savron03@gmail.com>
2025-01-11 11:32:57 +0100

    exposal te prave porte za docker

--- osebnospremljanje/storitve/src/main/java/si/skavtko/osebnospremljanje/zrna/KafkaClanConsumer.java
+++ osebnospremljanje/storitve/src/main/java/si/skavtko/osebnospremljanje/zrna/KafkaClanConsumer.java
@@ -6,7 +6,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
